8a6208320e44ae15ede148287dd4260cb5e0a542
League Student <league@iMac-14.attlocal.net>
2019-10-11 18:29:20 -0700

    Java Spiral

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -9,6 +9,42 @@ public class MyFirstJavaProgram {
 		
 		// START HERE
 	
+		Robot Mike = new Robot();
+		Mike.setSpeed(5);
+		
+		Mike.setPenColor(Color.blue);
+		
+		Mike.penDown(
+					);
+		Mike.move(100);
+		
+		Mike.turn(90);
+		
+		Mike.setPenColor(Color.ORANGE);
+		
+		Mike.move(100);
+		
+		Mike.turn(90);
+		
+		Mike.setPenColor(Color.BLACK);
+		
+		Mike.move(100);
+		
+		Mike.turn(90);
+		
+		Mike.setPenColor(Color.green);
+		
+		Mike.move(100);
+		
+		Mike.penUp();
+		
+		Mike.setSpeed(10);
+	
+		Mike.turn(90);
+		
+		Mike.move(-100);
+		
+		Mike.turn(100000);
 		
 		
 	}
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,26 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Drawer = new Robot();
 		// 5. Set your robot's pen down 
+		Drawer.penDown();
 		
 		// 3. Set the robot to go at max speed (100)
+		Drawer.setSpeed(100);
 		
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for(int i = 0 ; i < 76 ; i++ ) {
 		
 			// 7. Change the pen color to random
+		Drawer.setRandomPenColor();
 		
 			// 6. Move the robot 5 times the loop counter (5*i)
+		Drawer.move(5 * i); 
 			
 			// 2. Turn the robot 360/7 degrees to the right
+		Drawer.turn(360/7);
 		
 			// 8. Set the pen width to i
+		Drawer.setPenWidth(i);
 			
-	}
-}
+	}}}
