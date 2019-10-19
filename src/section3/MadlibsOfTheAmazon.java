05cf14b287ae2303e67989409cc6ae8144947fc4
League Student <league@iMac-9.attlocal.net>
2019-10-18 18:29:19 -0700

    Day 3

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -6,18 +6,19 @@ public class MadlibsOfTheAmazon {
 	
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
+			
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog (null ,"If you find yourself having to cross a savana with lots of tigers, here's how to do it..." );
 		// Get the user to enter an adjective
-
+		String adj = JOptionPane.showInputDialog("Type an adjective" );
 		// Get the user to enter a type of liquid
-
+		String liquid = JOptionPane.showInputDialog("Type a liquid" );
 		// Get the user to enter a body part
-
+		String bodyPart = JOptionPane.showInputDialog("Type in a body part" );
 		// Get the user to enter a verb
-
+		String verb = JOptionPane.showInputDialog("Type in a verb" );
 		// Get the user to enter a place
-
+		String place = JOptionPane.showInputDialog("Enter a place" );
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
@@ -26,8 +27,11 @@ public class MadlibsOfTheAmazon {
 		// back to the [place]. Good luck!
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
+		JOptionPane.showMessageDialog (null ,"Tigers are more " + adj + " during the day. So cross the river at night. Tigers are attracted to fresh "
+		+ liquid + " and will most likely bite out of your " + bodyPart + " if you " + verb + ". Whatever you do if you have and open wound, try to find another way to get to " + place +"." );
+			
+		}}
 		
-
-	}
-}
+	
+		
 
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,9 +6,16 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+		  String question = JOptionPane.showInputDialog ("Do you know how to code?" );
 		// 2. If they say "yes", tell them they will rule the world.
-
+		  if(question.equalsIgnoreCase("yes")){
+			JOptionPane.showMessageDialog(null, "You will rule the WORLD!!!!!!" );
+			
+		  } else {
+			  JOptionPane.showMessageDialog(null, "Better start until it's too late." );
+		  }
+		  
+		  
 		// 3. Otherwise, wish them good luck washing dishes.
 
 	}
