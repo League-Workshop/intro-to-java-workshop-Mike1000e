160828277f8be9172b971ec18aba6e7241506b0f
League Student <league@iMac-9.attlocal.net>
2019-10-25 18:30:20 -0700

    day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,24 +2,37 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+    Random rndmNmbr = new Random();
 	// 3. Print out this variable
-
+    int number = rndmNmbr.nextInt(4);
+    System.out.println(number);
 	// 4. Get the user to enter something that they think is awesome
-
+    String awsome = JOptionPane.showInputDialog(null, "What do you think is awsome?");
 	// 5. If the random number is 0
-
+    if (number == 0){
+    	JOptionPane.showMessageDialog(null, awsome + " is awsome!!");
+     }
 	// -- tell the user whatever they entered is awesome!
-
+    if (number == 1){
+    	JOptionPane.showMessageDialog(null, awsome + " is ok.");
+     }
 	// 6. If the random number is 1
-
+    if (number == 2){
+    	JOptionPane.showMessageDialog(null, awsome + " is so boring.");
+     }
 	// -- tell the user whatever they entered is ok.
-
+    if (number == 3){
+    	JOptionPane.showMessageDialog(null, awsome + " is the worst thing in the world!!!!");
+     }
 	// 7. If the random number is 2
 
 	// -- tell the user whatever they entered is boring.
@@ -28,6 +41,6 @@ public class AwesomeOrNot {
 
 	// -- write your own answer
 
-}
+}}
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,47 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+
+		int score = 0;
+		System.out.println(score);
 		// 2.  Ask the user a question 
+		String question1 = JOptionPane.showInputDialog(null, "What is the most subsribed YouTube chanell?");
 		
-		// 3.  Use an if statement to check if their answer is correct
+		if (question1.equalsIgnoreCase("tseries") || (question1.equalsIgnoreCase("t series"))) {
+			score = score ++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+			}
 		
-		// 4.  if the user's answer was correct, add one to their score 
+String question2 = JOptionPane.showInputDialog(null, "What is the YouTuber who wastes the most money?");
 		
+		if (question2.equalsIgnoreCase("mrbeast") || (question1.equalsIgnoreCase("mr beast"))) {
+			score = score ++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+			}
+String question3 = JOptionPane.showInputDialog(null, "What youtuber brought back Minecraft?");
+		
+		if  (question3.equalsIgnoreCase("Pewdiepie")) {
+			score = score ++;
+			JOptionPane.showMessageDialog(null, "Correct");
+		}else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+			}
+		
+		
+		// 4.  if the user's answer was correct, add one to their s
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
-	}
-}
+	
+}}
